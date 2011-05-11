4b6f225bdf028e23455198e46b69e6ba832ebe9f
Sergei O. Udalov <sergei.udalov@gmail.com>
2011-05-11 10:04:36 +0400

    tryin to create test account:w

--- app/models/facebook.rb
+++ app/models/facebook.rb
@@ -1,13 +1,13 @@
 require 'rubygems'
 require 'rest_client'
 
-  
-class Facebook 
- 
-  attr_accessor :token, :facebook_user, :friends, 
-                :oauth_url, :onlineFriends, :friendsRandom   
-  
-  def initialize(params = {})  
+
+class Facebook
+
+  attr_accessor :token, :facebook_user, :friends,
+                :oauth_url, :onlineFriends, :friendsRandom
+
+  def initialize(params = {})
     @token = getToken params
     @oauth_url = getOauthUrl params
 
@@ -18,32 +18,38 @@ class Facebook
     @friendsRandom = getFriendsRandom
   end
 
+  def createTestAccount
+    request_url = "https://graph.facebook.com/#{APP_ID}/accounts/test-users?installed=true&permissions=read_stream&method=post&access_token=#{@token}"
+    test_account = RestClient.get request_url
+    ActiveSupport::JSON.decode test_account
+  end
+
   # get facebook oauth url
   def getOauthUrl params
     api = Sutalk::Application.config.facebook_api
 
     if params[:sid].nil?
-      "#{api[:auth_url]}&redirect_uri=#{api[:canvas_page]}"    
+      "#{api[:auth_url]}&redirect_uri=#{api[:canvas_page]}"
     else
-      "#{api[:auth_url]}&redirect_uri=#{api[:canvas_page]}?sid=#{params[:sid]}"    
+      "#{api[:auth_url]}&redirect_uri=#{api[:canvas_page]}?sid=#{params[:sid]}"
     end
   end
-  
+
   # logged out? Returns true if user is logged out.
   def loggedout?
      token.nil?
   end
-      
+
   # get facebok user data
-  def getUser 
-    user = RestClient.get 'https://graph.facebook.com/me/', 
+  def getUser
+    user = RestClient.get 'https://graph.facebook.com/me/',
                           {:params => {:access_token => token}}
-    ActiveSupport::JSON.decode user    
+    ActiveSupport::JSON.decode user
   end
 
   # get user's facebook friends
   def getFriends
-    friends = RestClient.get 'https://graph.facebook.com/me/friends/', 
+    friends = RestClient.get 'https://graph.facebook.com/me/friends/',
                              {:params => {:access_token => token}}
     ActiveSupport::JSON.decode friends
   end
@@ -51,46 +57,46 @@ class Facebook
   # get online friends
   def getOnlineFriends
 
-    fql = "SELECT name, uid, first_name, profile_url, pic_square " + 
+    fql = "SELECT name, uid, first_name, profile_url, pic_square " +
           "FROM user WHERE online_presence " +
-          "IN ('active','idle') AND uid IN " + 
+          "IN ('active','idle') AND uid IN " +
           "(SELECT uid2 FROM friend WHERE uid1=me()" +
           "ORDER BY rand()" + ")"
-    
+
     facebook_api_fql_url = "https://api.facebook.com/method/fql.query"
-    
-    friends = RestClient.get facebook_api_fql_url, 
-                            {:params => {:access_token => token, 
-                                         :query => fql, 
+
+    friends = RestClient.get facebook_api_fql_url,
+                            {:params => {:access_token => token,
+                                         :query => fql,
                                          :format => "JSON"}}
     ActiveSupport::JSON.decode friends
   end
 
   # get all friends in random order
   def getFriendsRandom
-    fql = "SELECT name, uid, first_name, profile_url, pic_square " + 
+    fql = "SELECT name, uid, first_name, profile_url, pic_square " +
           "FROM user WHERE uid " +
-          " IN (SELECT uid2 FROM friend WHERE uid1 = me())"  + 
+          " IN (SELECT uid2 FROM friend WHERE uid1 = me())"  +
           "ORDER BY rand()"
-    
+
     facebook_api_fql_url = "https://api.facebook.com/method/fql.query"
-    
-    friends = RestClient.get facebook_api_fql_url, 
-                            {:params => {:access_token => token, 
-                                         :query => fql, 
+
+    friends = RestClient.get facebook_api_fql_url,
+                            {:params => {:access_token => token,
+                                         :query => fql,
                                          :format => "JSON"}}
     ActiveSupport::JSON.decode friends
   end
 
-  
-  protected 
-  
+
+  protected
+
   # get facebook token
-  def getToken(params = {})  
+  def getToken(params = {})
     # dataSignedRequest = decode_facebook_hash(params["signed_request"])
     dataSignedRequest = getSignedRequest(params["signed_request"])
     token = dataSignedRequest["oauth_token"]
-  end  
+  end
 
   # get signed request
   def getSignedRequest( signed_request )
--- public/javascripts/facebook.js
+++ public/javascripts/facebook.js
@@ -10,17 +10,17 @@
 var CANVAS_HEIGHT_SMALL = 600;
 var CANVAS_HEIGHT_LARGE = 1400;
 
-function inviteAllOnlineFriends(){ 
+function inviteAllOnlineFriends(){
 	setCanvasHeight(CANVAS_HEIGHT_SMALL);
 	getOnlineFriends();
 }
- 
+
 
 
 // Get online friends.  Returns an array of facebook user ids of online friends
 function getOnlineFriends(){
 	var fql = "SELECT uid FROM user WHERE online_presence IN ('active','idle') AND uid IN (SELECT uid2 FROM friend WHERE uid1=me())";
-	
+
 	var query = FB.Data.query(fql);
 
 	query.wait(function(rows) {
@@ -30,15 +30,15 @@ function getOnlineFriends(){
 
 // Check facebook permission for retrieving user's online friends' prescence
 function checkOnlinePrescencePermission(){
-	
+
 	FB.api(
 	  {
 	    method: 'fql.query',
 	    query: 'SELECT friends_online_presence FROM permissions WHERE uid = me()'
 	  },
-	  function(response) { 
+	  function(response) {
 			if(response[0].friends_online_presence == 0){
-				FB.login(function(response) {}, {perms:'friends_online_presence'});				
+				FB.login(function(response) {}, {perms:'friends_online_presence'});
 			}
 		}
 	);
@@ -46,28 +46,28 @@ function checkOnlinePrescencePermission(){
 
 function requestInvite( onlineFriends ){
 	var onlineFriendsArray = jsonToArrayOfValues(onlineFriends);
-	
+
 	var message = 'Please SUtalk video chat with me at ' +
 	 							fb_api.canvas_page + '?sid=' + opentok.session_id;
-	
-	FB.ui({	method: 'apprequests', 
+
+	FB.ui({	method: 'apprequests',
 					message: message,
-					title: 'Select Online Friends To SUtalk Video Chat', 
+					title: 'Select Online Friends To SUtalk Video Chat',
 					filters: [{name: 'Online Friends', user_ids: onlineFriendsArray}, 'all' ]
 				},
 				function(response) {
-						feedInvite();	
+						feedInvite();
 				}
 	);
 }
 
 // convert json object to an array of values
 function jsonToArrayOfValues(json){
-	var arr=new Array(); 
+	var arr=new Array();
 	for (i=0;i<json.length;i++){
 		arr.push(json[i].uid);
 	}
-	
+
 	return arr;
 }
 
@@ -82,14 +82,14 @@ feedInvite = function feedInvite(){
 	 							fb_api.canvas_page + '?sid=' + opentok.session_id
 	   },
 		 function(response) {
-				setCanvasHeight(CANVAS_HEIGHT_LARGE);	
+				setCanvasHeight(CANVAS_HEIGHT_LARGE);
 		 }
 	 );
 }
 
 // Set canvas height
 setCanvasHeight = function setCanvasHeight(h){
-	FB.Canvas.setSize({ height: h });		
+	FB.Canvas.setSize({ height: h });
 }
 
 // Invite a single friend to SUtalk video chat using a dialogue request
@@ -97,10 +97,10 @@ function inviteSingle(id){
 	setCanvasHeight(CANVAS_HEIGHT_SMALL);
 	var message = 'Please SUtalk video chat with me at ' +
 	 							fb_api.canvas_page + '?sid=' + opentok.session_id;
-	
-	FB.ui({	method: 'apprequests', 
+
+	FB.ui({	method: 'apprequests',
 					message: message,
-					title: 'Invite Friend To SUtalk Video Chat', 
+					title: 'Invite Friend To SUtalk Video Chat',
 					to: id
 				},
 				function(response) {
